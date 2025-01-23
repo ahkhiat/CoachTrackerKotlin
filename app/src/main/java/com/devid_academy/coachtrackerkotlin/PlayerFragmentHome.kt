50c85e4ff19877c37647f5bdb21ae8cd22387b2f
thierryleung <ahkhiat@hotmail.com>
2025-01-23 17:42:53 +0100

    profile en cours

--- app/src/main/java/com/devid_academy/coachtrackerkotlin/PlayerFragmentHome.kt
+++ app/src/main/java/com/devid_academy/coachtrackerkotlin/PlayerFragmentHome.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 
 
 class PlayerFragmentHome : Fragment() {
@@ -14,8 +16,16 @@ class PlayerFragmentHome : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_player_home, container, false)
+
+        val btnProfil: Button = view.findViewById(R.id.btn_profile_fg_player)
+        btnProfil.setOnClickListener {
+
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.fg_container, PlayerProfileFragment())
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+        return view
     }
-
-
 }
